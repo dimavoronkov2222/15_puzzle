5cf378eaa546d48b4dfe40a35c26146950ebb2f3
dimavoronkov2222 <dimavoronkov2@protonmail.com>
2025-02-09 14:11:36 +0100

    add files and add code

--- app/src/main/java/com/dmytro/a15puzzle/MainActivity.java
+++ app/src/main/java/com/dmytro/a15puzzle/MainActivity.java
@@ -2,10 +2,13 @@ package com.dmytro.a15puzzle;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.TextView;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -135,5 +138,19 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("elapsedTime", elapsedTime);
         intent.putExtra("moveCount", moveCount);
         startActivity(intent);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
